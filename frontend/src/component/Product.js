bae6ea89dfd2cd21e9e533e027ac05b12838dc5c
Md-Alqma <mdalqma7@gmail.com>
2023-06-23 21:39:32 +0530

    implemented add to cart functionality from home page and completed cart page functionalities

--- frontend/src/component/Product.js
+++ frontend/src/component/Product.js
@@ -2,9 +2,33 @@ import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Rating from "./Rating";
+import axios from "axios";
+import { useContext } from "react";
+import { Store } from "../Store";
 
 const Product = (props) => {
   const { product } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    if (existItem) {
+      window.alert("Product already in the cart");
+    }
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry, Product is out of stock");
+      return;
+    } else {
+      ctxDispatch({
+        type: "ADD_TO_CART",
+        payload: { ...item, quantity: 1 }, // payload: {...product, quantity} --> to see the product quantity in nav cart
+      });
+    }
+  };
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -16,7 +40,13 @@ const Product = (props) => {
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
-        <Button>Add to cart</Button>
+        {product.countInStock === 1 ? (
+          <Button variant="light" disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add to Cart</Button>
+        )}
       </Card.Body>
     </Card>
   );
--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,20 +1,40 @@
-import { useContext } from 'react';
-import { Store } from '../Store';
-import { Helmet } from 'react-helmet-async';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import MessageBox from '../component/MessageBox';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-
+import { useContext } from "react";
+import { Store } from "../Store";
+import { Helmet } from "react-helmet-async";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import MessageBox from "../component/MessageBox";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function CartScreen() {
+  const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
 
+  const updateCartHandler = async (item, quantity) => {
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry, Product is out of stock");
+      return;
+    } else {
+      ctxDispatch({
+        type: "ADD_TO_CART",
+        payload: { ...item, quantity }, // payload: {...product, quantity} --> to see the product quantity in nav cart
+      });
+    }
+  };
+  const removeItemHandler = (item) => {
+    ctxDispatch({ type: "REMOVE_CART_ITEM", payload: item });
+  };
+
+  const checkoutHandler = () => {
+    navigate("/signin?redirect=/shipping");
+  };
   return (
     <div>
       <Helmet>
@@ -36,25 +56,33 @@ export default function CartScreen() {
                       <img
                         src={item.image}
                         alt={item.name}
-                        className="img-fluid rounded img-thumbnail"
-                      ></img>{' '}
+                        className="img-fluid rounded img-thumbnail"></img>{" "}
                       <Link to={`/product/${item.slug}`}>{item.name}</Link>
                     </Col>
                     <Col md={3}>
-                      <Button variant="light" disabled={item.quantity === 1}>
-                        <i className="fas fa-minus-circle"></i>
-                      </Button>{' '}
-                      <span>{item.quantity}</span>{' '}
                       <Button
                         variant="light"
-                        disabled={item.quantity === item.countInStock}
-                      >
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity - 1)
+                        }
+                        disabled={item.quantity === 1}>
+                        <i className="fas fa-minus-circle"></i>
+                      </Button>{" "}
+                      <span>{item.quantity}</span>{" "}
+                      <Button
+                        variant="light"
+                        onClick={() =>
+                          updateCartHandler(item, item.quantity + 1)
+                        }
+                        disabled={item.quantity === item.countInStock}>
                         <i className="fas fa-plus-circle"></i>
                       </Button>
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant="light">
+                      <Button
+                        onClick={() => removeItemHandler(item)}
+                        variant="light">
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
@@ -70,7 +98,7 @@ export default function CartScreen() {
               <ListGroup variant="flush">
                 <ListGroup.Item>
                   <h3>
-                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{" "}
                     items) : $
                     {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
                   </h3>
@@ -80,8 +108,8 @@ export default function CartScreen() {
                     <Button
                       type="button"
                       variant="primary"
-                      disabled={cartItems.length === 0}
-                    >
+                      onClick={checkoutHandler}
+                      disabled={cartItems.length === 0}>
                       Proceed to Checkout
                     </Button>
                   </div>
