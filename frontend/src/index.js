a501c7237ee0d400fa7e226132b3669e21f22877
Md-Alqma <mdalqma7@gmail.com>
2023-07-03 21:52:57 +0530

    completed paypal payment integration in the app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import { StoreProvider } from "./Store";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
-        <PayPalScriptProvider deferLoading={true}>
+      <PayPalScriptProvider deferLoading={true}>
           <App />
         </PayPalScriptProvider>
       </HelmetProvider>
